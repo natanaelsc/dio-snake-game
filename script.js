53844c623c5462e72389bafb2c8aa18540ada5a2
Natanael Silva <natanael96@live.com>
2021-06-10 12:16:33 -0300

    Recarrega Página após Game Over

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function iniciarJogo() {
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(jogo)
             alert("Game Over :( ");
+            window.location.reload();
         }
     }
 
